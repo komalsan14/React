ec507188ccb394e76aeac862653791b5212b42a0
komal14sangtani <komal14sangtani@gmail.com>
2024-06-20 13:11:20 +0530

    ep-1

---
+++ App.js
@@ -0,0 +1,4 @@
+const heading=React.createElement("h1",{id:"heading"},"Hii");
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+//{} here this are object in this we can write attributes
